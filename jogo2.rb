b96450f3c1ed6b9774dd0c72feef7919d65896fd
SoberAnna <annap.0103@gmail.com>
2022-10-19 15:56:57 -0300

    esta varrendo o array corretamente

--- jogo2.rb
+++ jogo2.rb
@@ -1,19 +1,41 @@
-class SorteiaPalavra
-  attr_reader :palavra #não pode ser alterado pelo usuario
+class Palavra
 
-  File.open('palavras.txt', 'r') do |arq|
-    vetor = []
-    while line = arq.gets
-      vetor.push(line)
+  def sortear_palavra
+    
+  # attr_reader :palavra #não pode ser alterado pelo usuario
+  vetor = []
+    
+    File.open('palavras.txt', 'r') do |arq|
+      while line = arq.gets
+        vetor.push(line)
+      end
     end
+    
+    palavra = vetor.sample
   end
-  
-  palavra = @vetor
 
 end
 
-#testando separar a palavra
-palavra = "anna"
-letras = palavra.split('')
+# puts "digite uma letra: "
 
-puts letras
+# obj = gets.chomp
+
+# if obj != letras[0]
+#   puts "Errou"
+# else
+#   puts "opa"
+# end
+
+obj = Palavra.new
+
+vetor = obj.sortear_palavra.split('')
+
+puts vetor
+
+vetor.each do |letras|
+  if letras != "a"
+    puts "Errou"
+  else
+    puts "opa"
+  end
+end
