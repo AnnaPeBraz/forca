ecb2f280f105bf009f75afbf62fee9dd9871c414
SoberAnna <annap.0103@gmail.com>
2022-10-19 15:18:18 -0300

    Separando a palavra em um vetor de letras - ok

--- jogo2.rb
+++ jogo2.rb
@@ -1,8 +1,19 @@
-File.open('palavras.txt', 'r') do |arq|
-  vetor = []
-  while line = arq.gets
+class SorteiaPalavra
+  attr_reader :palavra #não pode ser alterado pelo usuario
+
+  File.open('palavras.txt', 'r') do |arq|
+    vetor = []
+    while line = arq.gets
       vetor.push(line)
+    end
   end
-  puts vetor
+  
+  palavra = @vetor
+
 end
 
+#testando separar a palavra
+palavra = "anna"
+letras = palavra.split('')
+
+puts letras
